dbda9c10a6b5b53f807f602bebc64741fae0fab8
Rodrigo <rodrigocosta@motoca.com.br>
2024-11-19 10:42:21 -0300

    transformando páginas em server components

--- src/app/sobre/mission/index.tsx
+++ src/app/sobre/mission/index.tsx
@@ -1,85 +1,43 @@
 'use client';
-import React, { useCallback, useEffect, useState } from 'react';
 import MissionCard from './_components/MissionCard';
-import api from '@/lib/axiosInstance';
 import DOMPurify from 'dompurify';
-import { Skeleton } from '@/components/ui/skeleton';
 
-interface MissionInfo {
-  data: {
-    id: string;
-    featured_description: string;
-    mission_description: string;
-    values_description: string;
-    created_at: string;
-    updated_at: string;
-  };
+interface IMissionProps {
+	featuredDescription: string;
+	missionDescription: string;
+	valuesDescription: string;
 }
 
-export default function Mission() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [mission, setMission] = useState<MissionInfo>();
-
-  const handleGetSustaintabilityInfo = useCallback(async () => {
-    try {
-      setIsLoading(true);
-      const res = await api.get<MissionInfo>(
-        '/api/without/about_mission_and_values/get'
-      );
-      setMission(res.data);
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setIsLoading(false);
-    }
-  }, []);
-
-  useEffect(() => {
-    handleGetSustaintabilityInfo();
-  }, [handleGetSustaintabilityInfo]);
-
-  return (
-    <section className="bg-primaryWhite py-28 lg:px-24 md:px-10 xl:px-48 w-full h-auto flex flex-col gap-11">
-      {isLoading ? (
-        <>
-          <Skeleton className="text-center w-full flex flex-col gap-7 items-center justify-center px-5 sm:px-0 bg-slate-200">
-            <Skeleton className="max-w-[879px] h-5" />
-            <Skeleton className="max-w-[879px] h-5" />
-          </Skeleton>
-          <Skeleton className="flex flex-col md:flex-row gap-12 items-center justify-center bg-slate-200">
-            <Skeleton className=" w-[350px] sm:w-[500px] h-[310px] rounded-[20px] bg-slate-300 pl-10 pt-10 flex flex-col gap-12 pr-2"></Skeleton>
-            <Skeleton className=" w-[350px] sm:w-[500px] h-[310px] rounded-[20px] bg-slate-300 pl-10 pt-10 flex flex-col gap-12 pr-2"></Skeleton>
-          </Skeleton>
-        </>
-      ) : mission ? (
-        <>
-          <div className="text-center w-full flex flex-col gap-7 items-center justify-center px-5 sm:px-0">
-            <h1 className="max-w-[879px] font-semibold text-2xl sm:text-4xl text-darkGray">
-              Nossa missão e valores
-            </h1>
-            <h2
-              className="max-w-[879px] font-medium text-base sm:text-lg text-lightGray"
-              dangerouslySetInnerHTML={{
-                __html: DOMPurify.sanitize(mission?.data.featured_description),
-              }}
-            />
-          </div>
-          <div className="flex flex-col md:flex-row gap-12 items-center justify-center">
-            <MissionCard
-              src={'./target-icon.svg'}
-              text={mission?.data.mission_description}
-              title="Missão"
-            />
-            <MissionCard
-              src={'./medal-icon.svg'}
-              text={mission?.data.values_description}
-              title="Valores"
-            />
-          </div>
-        </>
-      ) : (
-        <></>
-      )}
-    </section>
-  );
+export default function Mission({
+	featuredDescription,
+	missionDescription,
+	valuesDescription,
+}: IMissionProps) {
+	return (
+		<section className='bg-primaryWhite py-28 lg:px-24 md:px-10 xl:px-48 w-full h-auto flex flex-col gap-11'>
+			<div className='text-center w-full flex flex-col gap-7 items-center justify-center px-5 sm:px-0'>
+				<h1 className='max-w-[879px] font-semibold text-2xl sm:text-4xl text-darkGray'>
+					Nossa missão e valores
+				</h1>
+				<h2
+					className='max-w-[879px] font-medium text-base sm:text-lg text-lightGray'
+					dangerouslySetInnerHTML={{
+						__html: DOMPurify.sanitize(featuredDescription),
+					}}
+				/>
+			</div>
+			<div className='flex flex-col md:flex-row gap-12 items-center justify-center'>
+				<MissionCard
+					src={'./target-icon.svg'}
+					text={missionDescription}
+					title='Missão'
+				/>
+				<MissionCard
+					src={'./medal-icon.svg'}
+					text={valuesDescription}
+					title='Valores'
+				/>
+			</div>
+		</section>
+	);
 }
--- src/app/sobre/page.tsx
+++ src/app/sobre/page.tsx
@@ -1,42 +1,55 @@
-'use client';
-import Image from 'next/image';
 import AboutHeader from './header/AboutHeader';
 import Sustainability from './sustainabilitySection';
 import Mission from './mission';
 import AboutInstitutional from '@/components/sections/AboutInstitutional';
-import { useCallback, useEffect, useState } from 'react';
 import api from '@/lib/axiosInstance';
-import { IData } from '@/components/sections/Footer/components/ModalContacts';
+import WhatsAppBtn from '@/components/ui/WhatsAppBtn';
 
-export default function Sobre() {
-  const [companyData, setCompanyData] = useState<IData>({} as IData);
-  const getCompanyData = useCallback(async () => {
-    try {
-      const res = await api.get('/api/without/company_profile/get');
-      setCompanyData(res?.data?.data);
-    } catch (error) {
-      console.log(error);
-    }
-  }, []);
-  useEffect(() => {
-    getCompanyData();
-  }, [getCompanyData]);
-  return (
-    <div className="w-full h-full">
-      <AboutHeader />
-      <Sustainability />
-      <Mission />
-      <AboutInstitutional />
-      <div className="w-full h-full relative">
-        <Image
-          className="z-50 hover:cursor-pointer right-1 bottom-[25px] fixed animate-shakeWithPause"
-          src="/whatsapp-icon.svg"
-          alt="Ícone do whatsapp"
-          onClick={() => window.open(companyData?.whatsapp, '_blank')}
-          width={70}
-          height={70}
-        />
-      </div>
-    </div>
-  );
+export default async function Sobre() {
+	const linkData = await api.get('/api/without/company_profile/get');
+
+	const aboutHeaderData = await api.get('/api/without/about_header/get');
+
+	const sustainabilityData = await api.get('/api/without/about_sub_header/get');
+
+	const missionData = await api.get(
+		'/api/without/about_mission_and_values/get'
+	);
+
+	const aboutInstitutionalInfosData = await api.get(
+		'/api/without/home_institutional_section/index'
+	);
+
+	const aboutInstitucionalImageData = await api.get(
+		'/api/without/home_institutional_section/display_image'
+	);
+
+	return (
+		<div className='w-full h-full'>
+			<AboutHeader
+				base64={aboutHeaderData.data.data.base64}
+				title={aboutHeaderData.data.data.title}
+				description={aboutHeaderData.data.data.description}
+			/>
+			<Sustainability
+				base64={sustainabilityData.data.data.base64}
+				title={sustainabilityData.data.data.title}
+				description={sustainabilityData.data.data.description}
+			/>
+			<Mission
+				featuredDescription={missionData.data.data.featured_description}
+				missionDescription={missionData.data.data.mission_description}
+				valuesDescription={missionData.data.data.values_description}
+			/>
+			<AboutInstitutional
+				infos={{
+					link: aboutInstitutionalInfosData.data.data[0].link,
+					title: aboutInstitutionalInfosData.data.data[0].title,
+					description: aboutInstitutionalInfosData.data.data[0].description,
+				}}
+				image={aboutInstitucionalImageData.data}
+			/>
+			<WhatsAppBtn link={linkData.data.data.whatsapp} />
+		</div>
+	);
 }
