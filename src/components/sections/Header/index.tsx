6cfeff4a5bf32533ff08526a43b047bcf55b0bc0
Rodrigo <rodrigocosta@motoca.com.br>
2024-11-22 12:28:44 -0300

    modificando Tag Image para renderização da primeira imagem que é o maior componente da tela

--- src/components/sections/Header/index.tsx
+++ src/components/sections/Header/index.tsx
@@ -1,18 +1,18 @@
 'use client';
 
+import Image from 'next/image';
+
 export default function Header({ image }: { image: string }) {
-  return (
-    <div className="flex flex-col">
-      <div
-        className="w-full mt-[89px] text-white flex h-full flex-col"
-        style={{
-          backgroundImage: `url(${image})`,
-          aspectRatio: 1.9,
-          backgroundSize: '100%',
-          backgroundPosition: 'center 0',
-          backgroundRepeat: 'no-repeat',
-        }}
-      ></div>
-    </div>
-  );
+	return (
+		<div className='w-full text-white flex h-full flex-col'>
+			<Image
+				src={image}
+				alt='Imagem principal do header Íaça'
+				loading='lazy'
+				className='object-cover w-full h-full mt-[60px]'
+				width={0}
+				height={0}
+			></Image>
+		</div>
+	);
 }
