cd3806fe3f05d9bcd9b629f210395cdb4f74c05f
Rodrigo <rodrigocosta@motoca.com.br>
2024-11-29 17:40:31 -0300

    criação da ui de login

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,90 @@
+'use client';
+
+import { Input } from '@/components/ui/Input';
+import { Label } from '@/components/ui/Label';
+import Image from 'next/image';
+import logoIaca from '../../../../public/logo-iaca-purple.svg';
+import { Button } from '@/components/ui/Button';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+const schema = z.object({
+	email: z
+		.string()
+		.min(1, 'E-mail é obrigatório')
+		.email('Informe um e-mail válido'),
+	password: z.string().min(1, 'Senha é obrigatório'),
+});
+
+type FormData = z.infer<typeof schema>;
+
+export default function SignIn() {
+	const {
+		register,
+		formState: { errors },
+		handleSubmit: hookFormHandleSubmit,
+	} = useForm<FormData>({
+		resolver: zodResolver(schema),
+	});
+
+	const handleSubmit = hookFormHandleSubmit(async (data) => {
+		console.log(data);
+	});
+
+	return (
+		<div className='w-[700px] p-0'>
+			<form
+				onSubmit={handleSubmit}
+				className='bg-white rounded-md px-8 py-8 flex items-center justify-center flex-col absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[400px] shadow-md'
+			>
+				<Image src={logoIaca} alt='Logo IAÇA' width={70} height={60} />
+
+				<span className='font-medium text-sm'>
+					Olá para continuar, faça seu login
+				</span>
+
+				<div className='grid w-full items-center gap-1.5 mt-4 mb-4'>
+					<Label className='text-xs' htmlFor='email'>
+						E-mail
+					</Label>
+					<Input {...register('email')} id='email' placeholder='E-mail' />
+
+					{errors.email && (
+						<span className='text-red-400 text-xs'>{errors.email.message}</span>
+					)}
+				</div>
+
+				<div className='grid w-full items-center gap-1.5'>
+					<Label className='text-xs' htmlFor='password'>
+						Senha
+					</Label>
+					<Input
+						{...register('password')}
+						id='password'
+						placeholder='Senha'
+						type='password'
+					/>
+					{errors.password && (
+						<span className='text-red-400 text-xs'>
+							{errors.password.message}
+						</span>
+					)}
+				</div>
+
+				<div className='w-full flex justify-end'>
+					<span className='text-[#16A6FF] text-xs underline cursor-pointer mt-4'>
+						Criar meu cadastro
+					</span>
+				</div>
+
+				<Button
+					type='submit'
+					className='bg-[#2B0036] rounded-full mt-4 w-full hover:bg-[#421d4b]'
+				>
+					Login
+				</Button>
+			</form>
+		</div>
+	);
+}
--- src/components/sections/Products/components/FooterProducts.tsx
+++ src/components/sections/Products/components/FooterProducts.tsx
@@ -4,72 +4,74 @@ import { Button } from '@/components/ui/Button';
 import ToggleQuantity from '@/components/ui/ToggleQuantity';
 import { useCart } from '@/contexts/Cart/CartContext';
 import { ShoppingCart } from 'lucide-react';
+import { useRouter } from 'next/navigation';
 import { toast } from 'sonner';
 
 interface ICartProps {
-  code: string;
-  nameProduct: string;
-  urlImage: string;
+	code: string;
+	nameProduct: string;
+	urlImage: string;
 }
 
 export default function FooterProducts({
-  code,
-  nameProduct,
-  urlImage,
+	code,
+	nameProduct,
+	urlImage,
 }: ICartProps) {
-  const { quantity, handleTotalCart } = useCart();
+	const { quantity, handleTotalCart } = useCart();
 
-  function handleAddToCart() {
-    console.log(quantity);
+	const router = useRouter();
 
-    try {
-      const cart = {
-        codigoProduto: code,
-        nomeProduto: nameProduct,
-        urlImage: urlImage,
-        param: [
-          {
-            codigo_cliente: 0,
-            itens: [
-              {
-                codigo_produto: 0,
-                quantidade: quantity,
-                valor_unitario: 23,
-                cfop: '5.101',
-                codigo_cenario_impostos_item: 0,
-                dados_adicionais_item: 'Forma de pagamento',
-                obs_item: 'Texto de valor teste',
-              },
-            ],
-          },
-        ],
-      };
+	function handleAddToCart() {
+		router.push('/sign-in');
+		// try {
+		// 	const cart = {
+		// 		codigoProduto: code,
+		// 		nomeProduto: nameProduct,
+		// 		urlImage: urlImage,
+		// 		param: [
+		// 			{
+		// 				codigo_cliente: 0,
+		// 				itens: [
+		// 					{
+		// 						codigo_produto: 0,
+		// 						quantidade: quantity,
+		// 						valor_unitario: 23,
+		// 						cfop: '5.101',
+		// 						codigo_cenario_impostos_item: 0,
+		// 						dados_adicionais_item: 'Forma de pagamento',
+		// 						obs_item: 'Texto de valor teste',
+		// 					},
+		// 				],
+		// 			},
+		// 		],
+		// 	};
 
-      localStorage.setItem('cart', JSON.stringify(cart));
+		// 	localStorage.setItem('cart', JSON.stringify(cart));
 
-      handleTotalCart(quantity);
+		// 	handleTotalCart(quantity);
 
-      toast.success(
-        `${quantity} ${quantity > 1 ? 'itens' : 'item'} ${
-          quantity > 1 ? 'adicionados' : 'adicionado'
-        } ao seu carrinho!`
-      );
-    } catch (error) {
-      toast.error('Algo deu errado ao tentar adicionar item ao carrinho!');
-    }
-  }
+		// 	toast.success(
+		// 		`${quantity} ${quantity > 1 ? 'itens' : 'item'} ${
+		// 			quantity > 1 ? 'adicionados' : 'adicionado'
+		// 		} ao seu carrinho!`
+		// 	);
+		// } catch (error) {
+		// 	toast.error('Algo deu errado ao tentar adicionar item ao carrinho!');
+		// }
+	}
 
-  return (
-    <div className="flex flex-col px-5 md:flex-row justify-between gap-5 items-center w-full">
-      <ToggleQuantity />
+	return (
+		<div className='flex flex-col px-5 md:flex-row justify-between gap-5 items-center w-full'>
+			<ToggleQuantity />
 
-      <Button
-        className="rounded-full w-full md:w-[380px] h-[50px] bg-[#2B0036] hover:bg-[#421d4b]"
-        onClick={handleAddToCart}
-      >
-        Adicionar ao carrino
-        <ShoppingCart />
-      </Button>
-    </div>
-  );
+			<Button
+				className='rounded-full w-full md:w-[380px] h-[50px] bg-[#2B0036] hover:bg-[#421d4b]'
+				onClick={handleAddToCart}
+			>
+				Adicionar ao carrino
+				<ShoppingCart />
+			</Button>
+		</div>
+	);
 }
